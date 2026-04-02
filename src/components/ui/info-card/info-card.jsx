05ede5984e689f8c4a58ff556a7fd5ae3181a05b
omwi <bsuiromwi@gmail.com>
2026-04-02 17:27:15 +0300

    feat: add InfoCard component

---
+++ src/components/ui/info-card/info-card.jsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+import Card from '../card';
+import IconContainer from '../icon-container';
+import * as styles from './info-card.module.css';
+
+export default function InfoCard({ children, icon, title, className }) {
+  return (
+    <Card className={clsx(styles.infoCard, className)}>
+      <header>
+        <IconContainer size={36} className={styles.iconWrapper}>
+          {icon}
+        </IconContainer>
+        <h3>{title}</h3>
+      </header>
+      {children}
+    </Card>
+  );
+}
